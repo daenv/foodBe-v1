9745223e5d8691407204dffdbc9f8861a8f31794
An Ne` <daenvle69@gmail.com>
2022-12-25 17:45:29 +0700

    25/12/2022

---
+++ src/v1/config/mongoConnect.ts
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const URI = process.env.MONGO_URI;
+
+mongoose.set('strictQuery', false);
+//connect mongoose
+mongoose.connect(URI).then(() => console.log('Connected mongoose success!...'))
+    .catch(() => console.log('Connected mongoose failed!...'))
+// get mongodb-shell friendly output (ISODate)
+mongoose.set('debug', { shell: true })
+
+module.exports = mongoose;
